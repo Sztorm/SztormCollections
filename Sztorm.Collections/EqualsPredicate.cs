9ce2c798bc7c22caad2f8a73e3684a5c2014bddb
Sztorm <Marcinwicher99@gmail.com>
2020-03-17 20:31:17 +0100

    Added IPredicate interface for future use in methods which can take advantage of it. Added EqualsPredicate.

---
+++ Sztorm.Collections/EqualsPredicate.cs
@@ -0,0 +1,53 @@
+﻿/*
+ * MIT License
+ * 
+ * Copyright (c) 2020 Sztorm
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+using System;
+using System.Runtime.CompilerServices;
+
+namespace Sztorm.Collections
+{
+    /// <summary>
+    ///     Represents a predicate which use is to check equality.
+    /// </summary>
+    /// <typeparam name="T"><typeparamref name = "T"/> is <see cref="IEquatable{T}"/></typeparam>
+    public readonly struct EqualsPredicate<T> : IPredicate<T> where T : IEquatable<T>
+    {
+        public readonly T InnerObject;
+
+        /// <summary>
+        ///     Constructs a predicate which use is to check equality with object passed in.
+        /// </summary>
+        /// <param name="obj"></param>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public EqualsPredicate(T obj) => InnerObject = obj;
+
+        /// <summary>
+        ///     Returns a value indicating whether inner objects is equal to other.
+        /// </summary>
+        /// <param name="other"></param>
+        /// <returns></returns>
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public bool Invoke(T other) => InnerObject.Equals(other);
+    }
+}
