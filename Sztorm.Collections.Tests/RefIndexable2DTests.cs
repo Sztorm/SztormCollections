dfbc1796a893290b21342c5286608ac418db8d8f
Sztorm <Marcinwicher99@gmail.com>
2020-04-04 03:38:08 +0200

    Added RefReadonly variants for Indexable interfaces. Added tests for IIndexable2D Updated RefReadonlyRectangularCollection: renamed to RefReadOnlyRectangularCollection. Removed IColumn interface as it had no use.

---
+++ Sztorm.Collections.Tests/RefIndexable2DTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace Sztorm.Collections.Tests
+{
+    [TestFixture]
+    public class RefIndexable2DTests
+    {
+        public static void TestIndexer<T, TRefIndexable>(
+            TRefIndexable indexable, Index2D index, T expected)
+            where TRefIndexable : IRefIndexable2D<T> 
+            => Assert.AreEqual(expected, indexable[index]);
+
+        public static void IsValidIndexReturnsFalseOnInvalidIndexArguments<T, TRefIndexable>(
+            TRefIndexable indexable, Index2D index)
+            where TRefIndexable : IRefIndexable2D<T>
+            => Assert.False(indexable.IsValidIndex(index));
+    }
+}
--- Sztorm.Collections/IRefReadOnlyIndexable.cs
+++ Sztorm.Collections/IRefReadOnlyIndexable.cs
@@ -22,14 +22,16 @@
  * SOFTWARE.
  */
 
-using System;
-using System.Collections.Generic;
-using System.Text;
-
 namespace Sztorm.Collections
 {
-    public interface IColumn<T, TIndexable> : IEnumerable<T>
+    /// <summary>
+    /// Exposes an <see langword="ref readonly"/> indexer and a method checking it.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public interface IRefReadOnlyIndexable<T>
     {
+        ref readonly T this[int index] { get; }
 
+        bool IsValidIndex(int index);
     }
 }
