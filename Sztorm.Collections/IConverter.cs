d5cc2ae38ab5b9be9818c2292b0a1f467d2a0586
Sztorm <Marcinwicher99@gmail.com>
2020-04-10 00:46:19 +0200

    Added IConverter interface. Updated IPredicate docs.

--- Sztorm.Collections/IConverter.cs
+++ Sztorm.Collections/IConverter.cs
@@ -25,23 +25,22 @@
 namespace Sztorm.Collections
 {
     /// <summary>
-    ///     Represents interface with method that defines a set of criteria and determines whether
-    ///     the specified object meets those criteria.<br/>
-    ///     This interface mimics <see cref="System.Predicate{T}"/> behavior. If the interface is
-    ///     implemented by a <see langword="struct"/> and is used as constraint in generic method,
-    ///     the implemented method call is direct and the garbage collector's work is avoided.
+    ///     Represents interface which defines a method that converts an object from one type to
+    ///     another type.<br/>
+    ///     This interface mimics <see cref="System.Converter{TInput, TOutput}"/> behavior. If the
+    ///     interface implemented by a <see langword="struct"/> and is used as constraint in
+    ///     generic method, the implemented method call is direct and the garbage collector's work
+    ///     is avoided.
     /// </summary>
-    /// <typeparam name="T"></typeparam>
-    public interface IPredicate<in T>
+    /// <typeparam name="TInput">The type of object that is to be converted.</typeparam>
+    /// <typeparam name="TOutput">The type the input object is to be converted to.</typeparam>
+    public interface IConverter<in TInput, out TOutput>
     {
         /// <summary>
-        ///     Represents a method that defines a set of criteria and determines whether the
-        ///     specified object meets those criteria.
+        ///     Represents a method that converts an object from one type to another type.
         /// </summary>
-        /// <param name="obj">
-        ///     The object to compare against the criteria defined within the implemented method.
-        /// </param>
+        /// <param name="input">The object to convert.</param>
         /// <returns></returns>
-        bool Invoke(T obj);
+        TOutput Invoke(TInput input);
     }
 }
