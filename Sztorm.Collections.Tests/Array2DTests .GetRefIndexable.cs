8dfa800f15686a9eca7720c491adeb8db5b327b6
Sztorm <Marcinwicher99@gmail.com>
2020-04-23 15:36:21 +0200

    Added Array2DTests.GetIndexable class for GetRow and GetColumn tests and moved some of them from Array2DTests. Added Array2DTests.Properties class to test basic properties of Array2D.

---
+++ Sztorm.Collections.Tests/Array2DTests .GetRefIndexable.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework.Api;
+using NUnit.Framework;
+
+namespace Sztorm.Collections.Tests
+{
+    [TestFixture]
+    public partial class Array2DTests
+    {
+        public static class GetRefIndexable
+        {
+            [TestCaseSource(typeof(GetRefIndexable), nameof(GetRowInvalidTestCases))]
+            public static void GetRowThrowExceptionIfIndexExceedsRows<T>(
+                Array2D<T> array, int index)
+                => Assert.Throws<ArgumentOutOfRangeException>(() => array.GetRow(index));
+
+            [TestCaseSource(typeof(GetRefIndexable), nameof(GetRowTestCases))]
+            public static void GetRowTest<T>(
+                Array2D<T> array, int index, RefRow<T, Array2D<T>> expected)
+                => CollectionAssert.AreEqual(expected, array.GetRow(index));
+
+            [TestCaseSource(typeof(GetRefIndexable), nameof(GetColumnInvalidTestCases))]
+            public static void GetColumnThrowExceptionIfIndexExceedsColumns<T>(
+                Array2D<T> array, int index)
+                => Assert.Throws<ArgumentOutOfRangeException>(() => array.GetColumn(index));
+
+            [TestCaseSource(typeof(GetRefIndexable), nameof(GetColumnTestCases))]
+            public static void GetColumnTest<T>(
+                Array2D<T> array, int index, RefColumn<T, Array2D<T>> expected)
+                => CollectionAssert.AreEqual(expected, array.GetColumn(index));
+
+            private static IEnumerable<TestCaseData> GetRowInvalidTestCases()
+            {
+                yield return new TestCaseData(new Array2D<byte>(2, 1), -1);
+                yield return new TestCaseData(new Array2D<byte>(2, 1), 2);
+                yield return new TestCaseData(new Array2D<byte>(0, 1), 0);
+            }
+
+            private static IEnumerable<TestCaseData> GetRowTestCases()
+            {
+                var array2x3 = Array2D<int>.FromSystem2DArray(
+                    new int[,] { { 4, 3, 7 },
+                                 { 8, 9, 2 } });
+
+                var firstRow = new RefRow<int, Array2D<int>>(new Array2D<int>(1, 3), 0);
+                firstRow[0] = 4;
+                firstRow[1] = 3;
+                firstRow[2] = 7;
+
+                var lastRow = new RefRow<int, Array2D<int>>(new Array2D<int>(1, 3), 0);
+                lastRow[0] = 8;
+                lastRow[1] = 9;
+                lastRow[2] = 2;
+
+                yield return new TestCaseData(array2x3, 0, firstRow);
+                yield return new TestCaseData(array2x3, 1, lastRow);
+            }
+
+            private static IEnumerable<TestCaseData> GetColumnInvalidTestCases()
+            {
+                yield return new TestCaseData(new Array2D<byte>(1, 2), -1);
+                yield return new TestCaseData(new Array2D<byte>(1, 2), 2);
+                yield return new TestCaseData(new Array2D<byte>(1, 0), 0);
+            }
+
+            private static IEnumerable<TestCaseData> GetColumnTestCases()
+            {
+                var array2x3 = Array2D<int>.FromSystem2DArray(
+                    new int[,] { { 4, 8 },
+                                 { 3, 9 },
+                                 { 7, 2 } });
+
+                var firstColumn = new RefColumn<int, Array2D<int>>(new Array2D<int>(3, 1), 0);
+                firstColumn[0] = 4;
+                firstColumn[1] = 3;
+                firstColumn[2] = 7;
+
+                var lastColumn = new RefColumn<int, Array2D<int>>(new Array2D<int>(3, 1), 0);
+                lastColumn[0] = 8;
+                lastColumn[1] = 9;
+                lastColumn[2] = 2;
+
+                yield return new TestCaseData(array2x3, 0, firstColumn);
+                yield return new TestCaseData(array2x3, 1, lastColumn);
+            }
+        }      
+    }
+}
---
+++ Sztorm.Collections.Tests/Array2DTests.Properties.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework.Api;
+using NUnit.Framework;
+
+namespace Sztorm.Collections.Tests
+{
+    [TestFixture]
+    public partial class Array2DTests
+    {
+        public static class Properties
+        {
+            [TestCase(0)]
+            [TestCase(1)]
+            [TestCase(5)]
+            public static void TestRows(int rows)
+            {
+                var array = new Array2D<int>(rows, 1);
+
+                Assert.AreEqual(rows, array.Rows);
+                Assert.AreEqual(rows, array.Length1);
+            }
+
+            [TestCase(0)]
+            [TestCase(1)]
+            [TestCase(5)]
+            public static void TestColumns(int columns)
+            {
+                var array = new Array2D<int>(1, columns);
+
+                Assert.AreEqual(columns, array.Columns);
+                Assert.AreEqual(columns, array.Length2);
+            }
+
+            [TestCase(0, 0)]
+            [TestCase(0, 1)]
+            [TestCase(1, 0)]
+            [TestCase(4, 2)]
+            [TestCase(1, 6)]
+            public static void TestCount(int rows, int columns)
+                => Assert.AreEqual(rows * columns, new Array2D<int>(rows, columns).Count);
+
+            [TestCase(0, 0)]
+            [TestCase(0, 1)]
+            [TestCase(1, 0)]
+            [TestCase(4, 2)]
+            [TestCase(1, 6)]
+            public static void TestBoundaries(int rows, int columns)
+                => Assert.AreEqual(
+                    new Bounds2D(rows, columns), new Array2D<int>(rows, columns).Boundaries);
+        }
+    }
+}
