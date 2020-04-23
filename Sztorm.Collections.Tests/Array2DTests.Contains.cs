9b28b505ae12cae351632fc9497ae4ab9e05d37f
Sztorm <Marcinwicher99@gmail.com>
2020-04-23 13:41:06 +0200

    Updated ContainsTests to make them more readable and renamed the file to match the new class name.

--- Sztorm.Collections.Tests/Array2DTests.Contains.cs
+++ Sztorm.Collections.Tests/Array2DTests.Contains.cs
@@ -9,34 +9,34 @@ namespace Sztorm.Collections.Tests
 {
     public partial class Array2DTests
     {
-        public static class ContainsTests
+        public static class Contains
         {
-            [TestCaseSource(typeof(ContainsTests), nameof(ContainsTestCases))]
-            [TestCaseSource(typeof(ContainsTests), nameof(ContainsEquatableTestCases))]
-            [TestCaseSource(typeof(ContainsTests), nameof(ContainsComparableTestCases))]
-            public static bool TestContains<T>(Array2D<T> array, T value) => array.Contains(value);
+            [TestCaseSource(typeof(Contains), nameof(AnyTestCases))]
+            [TestCaseSource(typeof(Contains), nameof(EquatableTestCases))]
+            [TestCaseSource(typeof(Contains), nameof(ComparableTestCases))]
+            public static bool TestAny<T>(Array2D<T> array, T value) => array.Contains(value);
 
             [Test]
-            public static void ContainsEquatableThrowsExceptionIfItemIsNull()
+            public static void EquatableThrowsExceptionIfItemIsNull()
                 => Assert.Throws<ArgumentNullException>(
                     () => new Array2D<string>(0, 0).ContainsEquatable<string>(item: null));
 
-            [TestCaseSource(typeof(ContainsTests), nameof(ContainsEquatableTestCases))]
-            public static bool TestContainsEquatable<T>(Array2D<T> array, T value)
+            [TestCaseSource(typeof(Contains), nameof(EquatableTestCases))]
+            public static bool TestEquatable<T>(Array2D<T> array, T value)
                 where T : IEquatable<T>
                 => array.ContainsEquatable(value);
 
             [Test]
-            public static void ContainsComparableThrowsExceptionIfItemIsNull()
+            public static void ComparableThrowsExceptionIfItemIsNull()
                 => Assert.Throws<ArgumentNullException>(
                     () => new Array2D<string>(0, 0).ContainsComparable<string>(item: null));
 
-            [TestCaseSource(typeof(ContainsTests), nameof(ContainsComparableTestCases))]
-            public static bool TestContainsComparable<T>(Array2D<T> array, T value)
+            [TestCaseSource(typeof(Contains), nameof(ComparableTestCases))]
+            public static bool TestComparable<T>(Array2D<T> array, T value)
                 where T : IComparable<T>
                 => array.ContainsComparable(value);
 
-            private static IEnumerable<TestCaseData> ContainsTestCases()
+            private static IEnumerable<TestCaseData> AnyTestCases()
             {
                 var array3x2 = Array2D<object>.FromSystem2DArray(
                         new object[,] { { 2, 3 },
@@ -52,7 +52,7 @@ namespace Sztorm.Collections.Tests
                 yield return new TestCaseData(array3x2, 7).Returns(false);
             }
 
-            private static IEnumerable<TestCaseData> ContainsEquatableTestCases()
+            private static IEnumerable<TestCaseData> EquatableTestCases()
             {
                 var array3x2 = Array2D<string>.FromSystem2DArray(
                         new string[,] { { "2", "3" },
@@ -68,7 +68,7 @@ namespace Sztorm.Collections.Tests
                 yield return new TestCaseData(array3x2, "7").Returns(false);
             }
 
-            private static IEnumerable<TestCaseData> ContainsComparableTestCases()
+            private static IEnumerable<TestCaseData> ComparableTestCases()
             {
                 var array3x2 = Array2D<int>.FromSystem2DArray(
                         new int[,] { { 2, 3 },
