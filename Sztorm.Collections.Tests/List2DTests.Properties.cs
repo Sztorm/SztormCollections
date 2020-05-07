b22d217a8dca38c242d212f0f510ac7385516af9
Sztorm <Marcinwicher99@gmail.com>
2020-05-07 12:26:06 +0200

    Updated list property tests and moved them to new file that matches the class name. Removed old tests from List2DTests.

---
+++ Sztorm.Collections.Tests/List2DTests.Properties.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework.Api;
+using NUnit.Framework;
+
+namespace Sztorm.Collections.Tests
+{
+    [TestFixture]
+    public partial class List2DTests
+    {
+        public static class Properties
+        {
+            [TestCase(0)]
+            [TestCase(1)]
+            [TestCase(5)]
+            public static void TestRows(int rows)
+            {
+                var list = new List2D<int>();
+                list.AddRows(rows);
+
+                Assert.AreEqual(rows, list.Rows);
+                Assert.AreEqual(rows, list.Length1);
+            }
+
+            [TestCase(0)]
+            [TestCase(1)]
+            [TestCase(5)]
+            public static void TestColumns(int columns)
+            {
+                var list = new List2D<int>();
+                list.AddColumns(columns);
+
+                Assert.AreEqual(columns, list.Columns);
+                Assert.AreEqual(columns, list.Length2);
+            }
+
+            [TestCase(0, 0)]
+            [TestCase(0, 1)]
+            [TestCase(1, 0)]
+            [TestCase(4, 2)]
+            [TestCase(1, 6)]
+            public static void TestCount(int rows, int columns)
+            {
+                var list = new List2D<int>();
+                list.IncreaseBounds(rows, columns);
+
+                Assert.AreEqual(rows * columns, list.Count);
+            }
+
+            [TestCase(0, 0)]
+            [TestCase(0, 1)]
+            [TestCase(1, 0)]
+            [TestCase(4, 2)]
+            [TestCase(1, 6)]
+            public static void TestCapacity(int rows, int columns)
+            {
+                var capacity = new Bounds2D(rows, columns);
+                var list = new List2D<int>(capacity);
+
+                Assert.AreEqual(capacity, list.Capacity);
+            }
+
+            [TestCase(0, 0)]
+            [TestCase(0, 1)]
+            [TestCase(1, 0)]
+            [TestCase(4, 2)]
+            [TestCase(1, 6)]
+            public static void TestBoundaries(int rows, int columns)
+            {
+                var list = new List2D<int>(rows, columns);
+                list.IncreaseBounds(rows, columns);
+
+                Assert.AreEqual(new Bounds2D(rows, columns), list.Boundaries);
+            }
+
+            [TestCase(0, 0)]
+            [TestCase(0, 1)]
+            [TestCase(1, 0)]
+            [TestCase(4, 2)]
+            [TestCase(1, 6)]
+            public static void IsEmptyReturnsTrueIfListIsInitializedWithCapacity(
+                int rowsCap, int colsCap)
+                => Assert.That(new List2D<int>(rowsCap, colsCap).IsEmpty);           
+
+            [Test]
+            public static void IsEmptyReturnsTrueWhenListIsInitializedWithoutSpecifiedCapacity()
+                => Assert.That(new List2D<int>().IsEmpty);
+
+            [TestCaseSource(typeof(List2DTests), nameof(ListsOfValues))]
+            [TestCaseSource(typeof(List2DTests), nameof(ListsOfReferences))]
+            public static void IsEmptyReturnsTrueWhenListIsCleared<T>(List2D<T> list)
+            {
+                list.Clear();
+                Assert.That(list.IsEmpty);
+            }
+        }
+    }
+}
