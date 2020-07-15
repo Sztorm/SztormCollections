1b71c675b7965d71be3c3385a6ee292dce31ba77
Sztorm <marcinwicher99@gmail.com>
2020-07-16 00:16:19 +0200

    Removed license information from each source code file as MIT license does not require it (LICENSE.txt is still intact).

--- Sztorm.Collections/RectangularCollectionUtils.cs
+++ Sztorm.Collections/RectangularCollectionUtils.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Runtime.CompilerServices;
-using System.Diagnostics;
+﻿using System.Runtime.CompilerServices;
 
 namespace Sztorm.Collections
 {
