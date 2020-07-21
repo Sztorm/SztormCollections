0beb11302c02df53d57950d2bd9816d217d9cc7f
Sztorm <marcinwicher99@gmail.com>
2020-07-21 17:17:14 +0200

    Added template for rectangular collection extensions.

---
+++ Sztorm.Collections/Implementations/2D/Common/RectangularCollectionExtensions.generated.cs
@@ -0,0 +1,19 @@
+// // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // //
+// File is auto-generated. Do not modify as changes may be overwritten.
+// If you want to modify this file, edit template file with the same name and .tt extension.
+// // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // // //
+
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Diagnostics;
+
+namespace Sztorm.Collections.Extensions
+{
+    using static RectangularCollectionUtils;
+
+    public static partial class RectangularCollectionExtensions
+    {
+    }
+}
